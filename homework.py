dfa89c9627933c6db0043e5a01a238ccee7d2c8e
Andy <161436408+Andyl24061@users.noreply.github.com>
2024-04-22 14:09:29 +1200

    Create homework.py

---
+++ homework.py
@@ -0,0 +1,21 @@
+print("Welcome to Mad Libs!")
+
+name = input("what is your name?")
+print(name)
+
+adjective = input("Enter an adjective")
+print(adjective)
+
+verb = input("Enter a verb")
+print(verb)
+
+place = input("Enter a place")
+print(place)
+
+food = input("Enter a food")
+print(food)
+
+vehicle = input("Enter a vehicle")
+print(vehicle)
+
+print(name, "is a ", verb, "person to be around. They love to cook in", place,".", "Their favourite food is", food, "which they eat in", vehicle)
